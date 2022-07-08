084ea8c4f7a28a4698dce8f4e082ff8b2667e067
Lars <lars@Larss-MacBook-Pro.local>
2022-07-08 19:48:18 +0200

    Trim down laylout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,26 +1,3 @@
-<script>
-	import GitHub from '$lib/svg/GitHub.svelte';
-	import Twitter from '$lib/svg/Twitter.svelte';
-</script>
-
-<nav />
-
 <main>
 	<slot />
 </main>
-
-<style>
-	@import '../global.css';
-
-	nav {
-		width: 100%;
-		position: fixed;
-		top: 0;
-		left: 0;
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
-		background-color: #fafbfc;
-		z-index: 1;
-	}
-</style>
