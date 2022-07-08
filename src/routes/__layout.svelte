534ef897b510aff00760c2fe0efe46c727b8bbb6
Lars <lars@Larss-MacBook-Pro.local>
2022-07-08 20:00:21 +0200

    Readd styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,33 @@
+<script>
+	import GitHub from '$lib/svg/GitHub.svelte';
+	import Twitter from '$lib/svg/Twitter.svelte';
+</script>
+
+<nav />
+
 <main>
 	<slot />
 </main>
+
+<style>
+	@import '../global.css';
+	nav {
+		width: 100%;
+		position: fixed;
+		top: 0;
+		left: 0;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		background-color: #fafbfc;
+		z-index: 1;
+	}
+
+	main {
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		padding-top: 67px;
+		position: relative;
+	}
+</style>
